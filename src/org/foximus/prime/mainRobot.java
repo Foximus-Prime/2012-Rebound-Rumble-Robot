85545ae3a3b33d9f9a17e4ef0eb0798ba19618e8
Austin Hampton <austinh100@yahoo.com>
2012-02-13 15:42:07 -0600

    Robot drive update.

--- src/org/foximus/prime/mainRobot.java
+++ src/org/foximus/prime/mainRobot.java
@@ -12,6 +12,7 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.DriverStationLCD;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,6 +27,8 @@ public class mainRobot extends IterativeRobot {
     Jaguar FL = new Jaguar(2);
     Jaguar BR = new Jaguar(3);
     Jaguar FR = new Jaguar(4);
+    
+    
     
     Joystick joy1 = new Joystick(1);
     Joystick joy2 = new Joystick(2);
@@ -53,20 +56,60 @@ public class mainRobot extends IterativeRobot {
     public void teleopPeriodic() {
         //double magnitude, direction, rotation;
         //Create "deadzone" variables. Adjust threshold value to increase/decrease deadzone
-        double X2 = 0, Y1 = 0, X1 = 0, threshold = 15.0;
+        //double X2 = 0, Y1 = 0, X1 = 0, threshold = 15.0;
         while (true && isOperatorControl() && isEnabled()) // loop until change 
         {
-            
             /*magnitude = joy1.getMagnitude();
             direction = joy1.getDirectionDegrees();
             rotation = joy2.getX();*/
             //drive.mecanumDrive_Cartesian(magnitude, direction, rotation, 0); 
             //drive.mecanumDrive_Cartesian(joy1.getX(), joy1.getY(), joy1.getZ(), 0.0);
-            //drive.mecanumDrive_Polar(joy1.getX(), joy1.getY(), joy2.getX());
+            //drive.mecanumDrive_Cartesian(joy1.getX(), joy1.getY(), joy2.getX(), 180.0);
             //Timer.delay(0.005);
             
             
+            // ... or for two 2-axis joysticks do this (Halo):
+            double forward = -joy1.getY(); // push joystick1 forward to go forward
+            double right = joy1.getX(); // push joystick1 to the right to strafe right
+            double clockwise = joy2.getX(); // push joystick2 to the right to rotate clockwise
             
+            DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser2, 1, "Joy1 X:"+right+" Y:"+forward);
+            DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser4, 1, "Joy2 X:"+clockwise);
+            DriverStationLCD.getInstance().updateLCD();
+            
+            // note: the above can only do 2 degrees of freedom at a time.
+            // Button1 selects Halo or Arcade.
+            // ... or any other driver interface scheme you like.
+            // K is a tuning constant for the “rotate” axis sensitivity.
+            // Start with K=0, and increase it very slowly (do not exceed K=1)
+            // to find the right value after you’ve got fwd/rev and strafe working:
+            double K = 0.1;
+            clockwise = K*clockwise;
+            // OPTIONAL. If desired, use the gyro angle for field-centric control.
+            // "theta" is the gyro angle, measured CCW from the zero reference:
+            /*double temp = forward*cos(theta) - right*sin(theta);
+            right = forward*sin(theta) + right*cos(theta);
+            forward = temp;*/
+            // Now apply the inverse kinematic tranformation
+            // to convert your vehicle motion command
+            // to 4 wheel speed commands:
+            double front_left = forward + clockwise + right;
+            double front_right = forward - clockwise - right;
+            double rear_left = forward + clockwise - right;
+            double rear_right = forward - clockwise + right;
+            // Finally, normalize the wheel speed commands
+            // so that no wheel speed command exceeds magnitude of 1:
+            double max = Math.abs(front_left);
+            if (Math.abs(front_right)>max) max = Math.abs(front_right);
+            if (Math.abs(rear_left)>max) max = Math.abs(rear_left);
+            if (Math.abs(rear_right)>max) max = Math.abs(rear_right);
+            if (max>1)
+            {front_left/=max; front_right/=max; rear_left/=max; rear_right/=max;}
+            // You're done. Send these four wheel commands to their respective wheels
+            FL.set(front_left);
+            BL.set(rear_left);
+            FR.set(front_right);
+            BR.set(rear_right);
             
             
             /*
@@ -79,6 +122,10 @@ public class mainRobot extends IterativeRobot {
             */
 
             //Create "deadzone" for Y1
+            /*Y1 = joy1.getY();
+            X1 = joy1.getX();
+            X2 = joy2.getX();*/
+            /*
             if(Math.abs(joy1.getY()) > threshold)
                 Y1 = joy1.getY();
             else
@@ -93,7 +140,7 @@ public class mainRobot extends IterativeRobot {
                 X2 = joy2.getX();
             else
                 X2 = 0;
-            
+            *//*
             double max = 0.0;
             if(Math.abs(Y1 - X2 - X1) > max)
                 max = Math.abs(Y1 - X2 - X1);
@@ -109,6 +156,7 @@ public class mainRobot extends IterativeRobot {
             BR.set((Y1 - X2 + X1)/max);
             FL.set((Y1 + X2 + X1)/max);
             BL.set((Y1 + X2 - X1)/max);
+             */
         }
 
     }    
